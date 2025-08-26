30911bef6c3b60675ebe37fb1e1dda39858dbe19
Stephen Glass <stephen@stephen.glass>
2025-08-26 00:46:23 -0400

    feat: make base path dynamic for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/stephenglass/",
+  base: process.env.BASE_PATH || "/",
   plugins: [preact(), tailwindcss()],
 });
